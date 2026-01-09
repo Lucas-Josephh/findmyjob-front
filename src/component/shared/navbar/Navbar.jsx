448a39250d3d6c8827f57d294a66eee82870fe3b
lucas <lucas.joseph@imerir.com>
2026-01-09 14:31:52 +0100

    Add : Navbar component

---
+++ src/component/shared/navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import Suitcase from '../svg/Suitcase'
+import Arrow from '../svg/Arrow'
+import './Navbar.scss'
+import House from '../svg/House'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react'
+
+export default function Navbar() {
+    const path = useLocation().pathname;
+    const navbarRef = useRef(null);
+    const [navHeight, setNavbHeight] = useState(0);
+    
+    const links = [
+        { name: "Accueil", path: "/", icon: <House /> },
+    ];
+    
+    useEffect(() => {
+        setNavbHeight(navbarRef.current.offsetHeight);
+
+        const handleResize = () => {
+            setNavbHeight(navbarRef.current.offsetHeight);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <>
+            <div style={{height: navHeight}}></div>
+            <nav className="extranet-navbar" ref={navbarRef}>
+                <div className="extranet-navbar-logo">
+                    <div className="extranet-navbar-logo_svg"><Suitcase /></div>
+                    <span className="extranet-navbar-logo-title">Find<span className="extranet-navbar-logo-title_my">My</span>Job</span>
+                </div>
+
+                <div className="extranet-navbar-link">
+                {links.map(link => ( 
+                   <Link
+                        key={link.name}
+                        to={link.path}
+                        className={`extranet-navbar-link-content ${path == link.path ? 'active' : ''}`}
+                    >
+                        {link.icon} {link.name}
+                    </Link>
+                ))}
+                </div>
+                
+                <div className="extranet-navbar-profil">
+                    <div className="extranet-navbar-profil_initial">LJ</div>
+                    <Arrow />
+                </div>
+            </nav>
+        </>
+    )
+}
