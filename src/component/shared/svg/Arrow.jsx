e67340f862ce089b453f31e0edd3f41ea182ce4f
lucas <lucas.joseph@imerir.com>
2026-01-09 14:32:04 +0100

    Add : SVG component

---
+++ src/component/shared/svg/Arrow.jsx
@@ -0,0 +1,3 @@
+export default function Arrow() {
+    return <svg style={{verticalAlign: 'middle'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(148 163 184)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down w-4 h-4 text-slate-400 hidden sm:block"><path d="m6 9 6 6 6-6"></path></svg>
+}
