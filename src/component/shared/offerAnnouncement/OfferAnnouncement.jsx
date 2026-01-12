e9e4f6ae35ed303bc44d23933f047b4523bc0fd4
lucas <lucas.joseph@imerir.com>
2026-01-12 15:03:49 +0100

    Add : SVG on the OfferAnnouncement component

--- src/component/shared/offerAnnouncement/OfferAnnouncement.jsx
+++ src/component/shared/offerAnnouncement/OfferAnnouncement.jsx
@@ -1,3 +1,8 @@
+import Clock from '../svg/ClockSuitcase copy'
+import Companie from '../svg/Companie'
+import Location from '../svg/Location'
+import Salary from '../svg/Salary'
+import Suitcase from '../svg/Suitcase'
 import Tag from '../tag/Tag'
 import './OfferAnnouncement.scss'
 
@@ -7,16 +12,16 @@ export default function OfferAnnouncement() {
             <div className='extranet-offerAnnouncement'>
                 <div className='extranet-offerAnnouncement-title'>
                     <h3 className='extranet-offerAnnouncement-title_h3'>Test</h3>
-                    <span className='extranet-offerAnnouncement-title_date'>7 janv.</span>
+                    <span className='extranet-offerAnnouncement-title_date'><Clock size="12" />7 janv.</span>
                 </div>
-                <span className='extranet-offerAnnouncement_companie'>SuperHost</span>
+                <span className='extranet-offerAnnouncement_companie'><Companie />SuperHost</span>
                 <div className='extranet-offerAnnouncement-info'>
-                    <span className='extranet-offerAnnouncement-info_location'>Location</span>
+                    <span className='extranet-offerAnnouncement-info_location'><Location size="16" />Location</span>
                     <span><Tag tag="interim" /></span>
                     <span><Tag tag="sur site" /></span>
-                    <span className='extranet-offerAnnouncement-info_level'>Junior</span>
+                    <span className='extranet-offerAnnouncement-info_level'><Suitcase size="12" /> Junior</span>
                 </div>
-                <div className='extranet-offerAnnouncement_salary'>455€ - 4557€</div>
+                <div className='extranet-offerAnnouncement_salary'><Salary />455€ - 4557€</div>
             </div>
         </>
     )
