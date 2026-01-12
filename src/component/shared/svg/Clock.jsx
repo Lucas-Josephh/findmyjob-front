851c22ea14ae7f86cef85adfc5e9d3c2f281013f
lucas <lucas.joseph@imerir.com>
2026-01-12 15:02:52 +0100

    Add : Other SVG

---
+++ src/component/shared/svg/Clock.jsx
@@ -0,0 +1,3 @@
+export default function Clock({size=20}) {
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-clock w-3 h-3"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+}
---
+++ src/component/shared/svg/Target.jsx
@@ -0,0 +1,3 @@
+export default function Target({size}) {
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-target w-8 h-8 text-white"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="6"></circle><circle cx="12" cy="12" r="2"></circle></svg>
+}
