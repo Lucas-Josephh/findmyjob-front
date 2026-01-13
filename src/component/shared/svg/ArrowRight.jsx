db41b083a79400dd1d594904aac532239ba2b355
lucas <lucas.joseph@imerir.com>
2026-01-13 10:07:46 +0100

    Add : new SVG

--- src/component/shared/svg/ArrowRight.jsx
+++ src/component/shared/svg/ArrowRight.jsx
@@ -1,3 +1,3 @@
 export default function ArrowRight({size=16}) {
-    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-arrow-right w-4 h-4 ml-2 group-hover:translate-x-1 transition-transform"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right w-4 h-4 ml-2 group-hover:translate-x-1 transition-transform"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
 }
--- src/component/shared/svg/Salary.jsx
+++ src/component/shared/svg/Salary.jsx
@@ -1,3 +1,3 @@
 export default function Salary({size=16}) {
-    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-banknote w-4 h-4"><rect width="20" height="12" x="2" y="6" rx="2"></rect><circle cx="12" cy="12" r="2"></circle><path d="M6 12h.01M18 12h.01"></path></svg>
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-banknote w-4 h-4"><rect width="20" height="12" x="2" y="6" rx="2"></rect><circle cx="12" cy="12" r="2"></circle><path d="M6 12h.01M18 12h.01"></path></svg>
 }
