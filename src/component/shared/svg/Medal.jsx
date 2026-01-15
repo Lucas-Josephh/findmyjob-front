f6cddf3a13ef779e44f75862fad2c043aab14d7a
lucas <lucas.joseph@imerir.com>
2026-01-15 17:46:19 +0100

    Add : SVG component

---
+++ src/component/shared/svg/Medal.jsx
@@ -0,0 +1,3 @@
+export default function Medal({size=16}) {
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-award w-5 h-5 text-sky-600"><path d="m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526"></path><circle cx="12" cy="8" r="6"></circle></svg>
+}
