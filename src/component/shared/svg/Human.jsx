77847e5ae7c26434befdd0fb572efbc95c14d09c
lucas <lucas.joseph@imerir.com>
2026-01-20 15:36:13 +0100

    Add : Connection / login component

---
+++ src/component/shared/svg/Human.jsx
@@ -0,0 +1,3 @@
+export default function Human({size=20}) {
+    return <svg style={{verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user w-8 h-8 text-white"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+}
