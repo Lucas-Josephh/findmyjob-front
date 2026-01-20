54db22e4906c6f4338b82e54ce0454fd8b9a1769
lucas <lucas.joseph@imerir.com>
2026-01-20 19:17:23 +0100

    Add : Langage knowledge model

---
+++ src/component/models/LanguageEnum.jsx
@@ -0,0 +1,9 @@
+const LanguageEnum = {
+  BEGINNER: "Débutant",
+  INTERMEDIATE: "Intermédiaire",
+  ADVANCED: "Avancé",
+  BILINGUAL: "Bilingue",
+  NATIVE: "Natif"
+};
+
+export default LanguageEnum;
