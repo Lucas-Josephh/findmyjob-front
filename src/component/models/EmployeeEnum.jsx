ba9878b32b557663d17d2f891c20e337d257d684
lucas <lucas.joseph@imerir.com>
2026-01-21 11:57:41 +0100

    Add : CompanyStep2 component

---
+++ src/component/models/EmployeeEnum.jsx
@@ -0,0 +1,8 @@
+const EmployeeEnum = {
+  EMPLOYEE_SIX: "1 - 6 employés",
+  EMPLOYEE_FIFTY: "6 - 50 employés",
+  EMPLOYEE_ONE_HUNDRED_AND_FIFTY: "50 - 150 employés",
+  EMPLOYEE_ONE_HUNDRED_AND_FIFTY_PLUS: "150+ employés"
+};
+
+export default EmployeeEnum;
---
+++ src/component/shared/connection/company/companyStep2/CompanyStep2.jsx
@@ -0,0 +1,63 @@
+import Suitcase from "../../../svg/Suitcase"
+import ActivitieEnum from "../../../../models/ActivitieEnum"
+import EmployeeEnum from "../../../../models/EmployeeEnum"
+import Select from "../../../select/Select"
+import Option from "../../../select/option/Option"
+import "./CompanyStep2.scss"
+
+export default function CompanyStep2({handleRemoveCompany, stepCompany}) {
+    return (
+        <div className="extranet-companyStep2">
+            <div className="extranet-companyStep2-header">
+                <span className="extranet-companyStep2-header_page">Étape {stepCompany}/2</span>
+                <div className="extranet-companyStep2-header-bar">
+                    <div className="extranet-companyStep2-header-bar_complete"></div>
+                    <div className="extranet-companyStep2-header-bar_complete"></div>
+                </div>
+            </div>
+
+            <div className="extranet-companyStep2-body">
+                <div className="extranet-companyStep2-body-text">
+                    <div className="extranet-companyStep2-body-text_logo"><Suitcase size="24" /></div>
+                    <h1 className="extranet-companyStep2-body-text_title">Détails supplémentaires</h1>
+                    <span className="extranet-companyStep2-body-text_subtitle">Aidez les candidats à mieux vous connaître</span>
+                </div>
+
+                <div className="extranet-companyStep2-body-container">
+                    <label className="extranet-companyStep2-body-container_label" htmlFor="">Secteur d'activité *</label>
+                    <Select className="app-publication-form-info-body-group_select" placeholder={"Sélectionnez un secteur"}>
+                        {Object.values(ActivitieEnum).map((value) => {
+                            return <Option className="app-publication-form-info-body-group_option" key={value}>{value}</Option>
+                        })}
+                    </Select>
+                </div>
+
+                <div className="extranet-companyStep2-body-container">
+                    <label className="extranet-companyStep2-body-container_label" htmlFor="">Nombre d'employés *</label>
+                    <Select className="app-publication-form-info-body-group_select" placeholder={"Sélectionnez un secteur"}>
+                        {Object.values(EmployeeEnum).map((value) => {
+                            return <Option className="app-publication-form-info-body-group_option" key={value}>{value}</Option>
+                        })}
+                    </Select>
+                </div>
+
+                <div className="extranet-companyStep2-body-container">
+                    <label className="extranet-companyStep2-body-container_label" htmlFor="">Email de contact (optionnel)</label>
+                    <input className="extranet-companyStep2-body-container_input" type="text" name="" id="" placeholder="contact@entreprise.com" />
+                </div>
+
+                <div className="extranet-companyStep2-body-container">
+                    <label className="extranet-companyStep2-body-container_label" htmlFor="">Téléphone (optionnel) (optionnel)</label>
+                    <input className="extranet-companyStep2-body-container_input" type="text" name="" id="" placeholder="+33 1 23 45 67 89" />
+                </div>
+
+                <div className="extranet-companyStep2-body-btn">
+                    <button className="extranet-companyStep2-body-btn_back" onClick={handleRemoveCompany}>&lt; Retour</button>
+                    <div className="extranet-companyStep2-body-btn-container">
+                        <button className="extranet-candidatStep3-body-btn_finish">Terminer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
