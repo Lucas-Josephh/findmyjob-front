9a4e0359e3eb2f922dde077ed1e7206015217497
lucas <lucas.joseph@imerir.com>
2026-01-21 11:58:00 +0100

    Add : Steps component

--- src/component/shared/connection/Connection.jsx
+++ src/component/shared/connection/Connection.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react"
 import Human from "../svg/Human"
 import Companie from "../svg/Companie"
-import "./Connection.scss"
-import { useState } from "react"
 import CandidatStep1 from "./candidat/candidatStep1/CandidatStep1";
 import CandidatStep2 from "./candidat/candidatStep2/CandidatStep2";
 import CandidatStep3 from "./candidat/candidatStep3/CandidatStep3";
+import CompanyStep1 from "./company/companyStep1/CompanyStep1"
+import CompanyStep2 from "./company/companyStep2/CompanyStep2";
+import "./Connection.scss"
 
 export default function Connection() {
     const [stepCandidat, setStepCandidat] = useState(0);
+    const [stepCompany, setStepCompany] = useState(0);
 
     const handleAddCandidat = () => {
+        setStepCompany(0);
         setStepCandidat(stepCandidat+1);
     }
 
+    const handleAddCompany = () => {
+        setStepCandidat(0);
+        setStepCompany(stepCompany+1);
+    }
+
     const handleRemoveCandidat = () => {
+        setStepCompany(0);
         setStepCandidat(stepCandidat-1);
+    }
+
+    const handleRemoveCompany = () => {
+        setStepCandidat(0);
+        setStepCompany(stepCompany-1);
     }
 
     return (
@@ -27,19 +42,23 @@ export default function Connection() {
                 <CandidatStep2 stepCandidat={stepCandidat} handleAddCandidat={handleAddCandidat} handleRemoveCandidat={handleRemoveCandidat} />
             : stepCandidat == 3 ?
                 <CandidatStep3 stepCandidat={stepCandidat} handleAddCandidat={handleAddCandidat} handleRemoveCandidat={handleRemoveCandidat} />
+            : stepCompany == 1 ?
+                <CompanyStep1 stepCompany={stepCompany} handleAddCompany={handleAddCompany} handleRemoveCompany={handleRemoveCompany}  />
+            : stepCompany == 2 ?
+                <CompanyStep2 stepCompany={stepCompany} handleRemoveCompany={handleRemoveCompany}  />
             :
                 <div className="module-connexion">
                     <h1 className="module-connexion_title">Bienvenue sur FindMyJob</h1>
                     <span className="module-connexion_subtitle">Commencez par choisir votre profil</span>
 
-                    <div className="module-connexion-container" onClick={handleAddCandidat}>
-                        <div className="module-connexion-container-candidat">
+                    <div className="module-connexion-container">
+                        <div className="module-connexion-container-candidat" onClick={handleAddCandidat}>
                             <div className="module-connexion-container-candidat_logo"><Human size="32" /></div>
                             <h3 className="module-connexion-container-candidat_title">Candidat</h3>
                             <span className="module-connexion-container-candidat_subtitle">Je cherche un emploi</span>
                         </div>
 
-                        <div className="module-connexion-container-companie">
+                        <div className="module-connexion-container-companie" onClick={handleAddCompany}>
                             <div className="module-connexion-container-companie_logo"><Companie size="32" /></div>
                             <h3 className="module-connexion-container-companie_title">Entreprise</h3>
                             <span className="module-connexion-container-companie_subtitle">Je recrute des talents</span>
